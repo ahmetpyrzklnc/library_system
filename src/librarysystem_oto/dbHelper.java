5188abbea1ae6932e6c929ed960fc2282e9effb2
Ahmet Kilinc <ahmtklnc.software@gmail.com>
2023-03-28 00:17:54 +0300

    database helper added!

---
+++ src/librarysystem_oto/dbHelper.java
@@ -0,0 +1,19 @@
+package LibrarySystem;
+
+import java.sql.*;
+
+public class dbHelper {
+    private String username = "root";
+    private String password = "1234";
+    private String dbUrl = "jdbc:mysql://localhost:3306/library_systemdb";
+    
+    public Connection getConnection() throws SQLException {
+        return (Connection) DriverManager.getConnection(dbUrl, username, password);
+    }
+    
+    public void showErrorMessage(SQLException exception){
+        System.out.println("Error: "+exception.getMessage());
+        System.out.println("Error Code:"+ exception.getErrorCode());
+    }
+
+}
